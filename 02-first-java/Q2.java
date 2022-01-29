48eaf25afee9efce16f58dab7b3fd8a6385b3b33
Joel James <53299215+Lyon03@users.noreply.github.com>
2022-01-29 18:05:41 +0530

    Corrected the naming to Q2

--- 02-first-java/Q2.java
+++ 02-first-java/Q2.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Q3 {
+public class Q2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner( System.in );
         String usersName;
